299e71b67b7aaab82e6c918974960efa586a32c4
Thomas Stratmann <thomas.stratmann@9elements.com>
2012-05-01 00:50:57 +0200

    thought usage over

--- usage_sketch.rb
+++ usage_sketch.rb
@@ -1,74 +1,66 @@
-# we have two objects [obj1, obj2] with predicate methods
+# we have three objects [obj1, obj2, obj3] with predicate methods
 
 @registered_req = AttachListen::Requirement.to_be(
     obj1.method(:in_desired_state1?),
-    obj2.method(:in_desired_state2?).tap do |req|
+    obj2.method(:in_desired_state2?) do |req|
   req.when_met { ... do something ... }
   req.when_unmet { ... do something else ... }
-  req.on_exception { |exception, occasion| ... inform the police ... }
+  req.instrument do |req, occasion, &block| #block is when_met/when_unmet block
+    # possibly log, meature time, ...
+    # some provision (yet to be invented) gives you interesting infos on req for debugging
+    block.call
+    # catch exception if you want (won't bubble up anyway)
+  end
 
   # with the setup so far, after the final connect, whe when_met/when_unmet would be called
   # exactly when the ANDed conditions _become_ true/false.
 
-  # If we do this in addition:
-  req.treat_disconnected_as_unmet!
-  # then in addition
-  # * the when_met block is called at connect time if ANDed conditions are true then
-  # * then when_unmet block is called at disconnect time if ANDed conditions are true then
-end.connect
+  #we have several modes:
 
-...
+  req.mode :evented
+  #this is the default. when_met/when_unmet blocks are called when the requirement becomes met/unmet.
 
-@registered_req.disconnect
-@registered_req = nil
+  req.mode :evented_plus #sorry for the name.
+  #same as above, plus:
+  # * if the requirement is already met at connect time, when_met block is called
+  # * if the requirement is met, when_unmet is called on .disconnect
+
+  req.mode :stateful
+  #in short, when_met and when_unmet behave like construction/tear down.
+  #
+  #requirement is only considered to be met when all dependent conditions are true and when_met block
+  #has been called successfully.
+  #when the first condition becomes false or on .disconnect,
+  #when_unmet block is NOT called unless requirement is in met state.
+  #The requirement provides its state as an attachable predicate, so you can nest requirements for refinement.
 
 
-# How to treat intermediate failure (e.g. exceptions in when_met block):
-@setup_in_met = false
+  req.dispatch_with(dispatcher) # ??
+
+end # <- connection is established after config block is left
+
+@registered_req.disconnect #detach from providers. help your garbage collector!
+
+
+# Stateful example with refinement
 @registered_req = AttachListen::Requirement.to_be(obj1.method(:in_desired_state1?),
-    obj2.method(:in_desired_state2?).tap do |req|
+    obj2.method(:in_desired_state2?) do |req|
   req.when_met do
-    unless @setup_in_met
       ...
-      @setup_in_met = true
-    end
   end
   req.when_unmet do
-    if @setup_in_met
       ...
-      @setup_in_met = false
-    end
   end
+  req.mode :stateful
 
-  req.treat_disconnected_as_unmet!
-end.connect
-# Do we want more sugar for this case? What would that look like?
-
-# Sugar variant
-@setup_in_met = false
-@registered_req = AttachListen::Requirement.to_be(obj1.method(:in_desired_state1?),
-    obj2.method(:in_desired_state2?).tap do |req|
-  req.when_met do
-    unless @setup_in_met
-      ...
-      @setup_in_met = true
-    end
-  end
-  req.when_unmet do
-    if @setup_in_met
-      ...
-      @setup_in_met = false
-    end
-  end
-
-  req.stateful! #implies .treat_disconnected_as_unmet
-
-  #now we are stateful, we can
-  req.refined_to_be(obj3.method(:in_desired_stage)).tap do |ref_req|
+  #sugar variant
+  req.refined_to_be(obj3.method(:in_desired_stage)) do |ref_req|
     ...
-  end.connect
+  end
+
   #which is roughly equivalent to:
-  AttachListen::Requirement.to_be(req.state,obj3.method(:in_desired_stage)).tap do |ref_req|
+  AttachListen::Requirement.to_be(req.method(:state), obj3.method(:in_desired_stage)).tap do |ref_req|
+    ref_req.mode :stateful
     ...
   end.connect
 
@@ -76,9 +68,11 @@ end.connect
 
 # How we can hide complex logic:
 
-machine = AttachListen::Machine.from_inputs(obj1.method(:parent)) #or
+machine = AttachListen::Machine.from_inputs(obj1.method(:parent)) do
+  provider_value
+end
 
-machine = AttachListen::Machine.from_inputs(:master_parent => obj1.method(:parent), :app_state => obj2.method(:state)) do |mac|
+machine = AttachListen::Machine.from_inputs(:master_parent => obj1.method(:parent), :app_state => obj2.method(:state)) do
   @inner_state_var = 0 #we're inside an object this time
 
   #access to state:
@@ -87,10 +81,13 @@ machine = AttachListen::Machine.from_inputs(:master_parent => obj1.method(:paren
 
   # ... heavy calculation ...
 
+  #initial state
   become(bool)
 
   on_master_parent_change do |new_value|
     # ... heavy calculation ...
+
+    #publish state change
     become_true
     become_false
     become(bool)
@@ -99,24 +96,14 @@ machine = AttachListen::Machine.from_inputs(:master_parent => obj1.method(:paren
   # ...
 end
 
-# I actually want to get rid of the .connect stuff.
+#easier configuration:
 
-@registered_req = AttachListen::Requirement.to_be(obj1.method(:in_desired_state1?),
-    obj2.method(:in_desired_state2?) do |req|
-  req.when_met { ... do something ... }
-  req.when_unmet { ... do something else ... }
-  req.on_exception { |which| ... inform the police ... }
+AttachListen.requirement_configuration(:token) do |config|
+  config.mode :stateful
 
-  req.treat_disconnected_as_unmet!
-  req.dispatch_with(dispatcher) #??
-end
-
-@registered_req.disconnect
-@registered_req.connect
-
-# state local configuration:
-
-AttachListen.configured do |config|
   config.treat_disconnected_as_unmet!
-  config.on_exception { |exception| ... }
+
+  config.instrument { ... }
 end
+
+AttachListen::Requirement.to_be(..., :config => :token) { ... }
